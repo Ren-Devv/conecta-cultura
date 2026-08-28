9c3ef082df89deea4737b2cb49c7c832cd82af4b
Lukas-ren <lu.duranh@duocuc.cl>
2026-08-28 15:18:08 -0400

    archivo js modificado con modulos JavaScript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,30 @@
-const botonBienvenida = document.querySelector("#boton-bienvenida");
+const nombreActividad = "Taller de fotografía";
+const capacidad = 30;
+let inscritos = 18;
 
-if (botonBienvenida) {
-  botonBienvenida.addEventListener("click", () => {
-    alert("Conecta Cultura se construye paso a paso.");
-  });
+console.log(nombreActividad);
+console.log(capacidad);
+console.log(inscritos);
+
+const cuposDisponibles = capacidad - inscritos;
+
+console.log(
+  `Quedan ${cuposDisponibles} cupos disponibles`
+);
+
+function calcularCupos(capacidad, inscritos) {
+  const disponibles = capacidad - inscritos;
+  return disponibles;
 }
+
+const cuposTaller = calcularCupos(30, 18);
+console.log(cuposTaller);
+
+const actividades = [
+  "Taller de fotografía",
+  "Concierto comunitario",
+  "Muestra de teatro"
+];
+
+console.log(actividades[0]);
+console.log(actividades[1]);
